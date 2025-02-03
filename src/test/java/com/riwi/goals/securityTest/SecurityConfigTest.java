cea15123a631021fa10284b1f2a780f0ec59777d
Emanuel Osorio <135929894+EmanuelOH@users.noreply.github.com>
2025-02-03 17:32:03 -0500

    test: add annotations for security tests configuration (#8)
    * fix: security test fix
    
    * test: add annotations for security tests configuration
    
    ---------
    
    Co-authored-by: AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>

--- src/test/java/com/riwi/goals/securityTest/SecurityConfigTest.java
+++ src/test/java/com/riwi/goals/securityTest/SecurityConfigTest.java
@@ -5,6 +5,7 @@ import com.riwi.goals.application.mappers.GoalMapper;
 import com.riwi.goals.application.services.impl.GoalService;
 import com.riwi.goals.domain.entities.Goal;
 import com.riwi.goals.domain.enums.Status;
+import com.riwi.goals.infraestructure.config.SecurityConfig;
 import com.riwi.goals.infraestructure.persistence.GoalRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -17,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -29,8 +31,9 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
+@Import(SecurityConfig.class)
 @ExtendWith(MockitoExtension.class)
 @DisplayName("Security Tests")
 public class SecurityConfigTest {
